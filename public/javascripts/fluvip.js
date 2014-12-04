85cf53cbfd2f589a22e8ae92034f1eb344cbb0a3
Angel Botto <angelbotto@gmail.com>
2014-12-03 19:27:08 -0500

    responsive

--- public/javascripts/fluvip.js
+++ public/javascripts/fluvip.js
@@ -1,14 +1,6 @@
 $(function() {
-
-    var header = $("#principal-nav");
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll >= 100) {
-            header.removeClass('navbar-transparent').addClass("navbar-slide");
-        } else {
-            header.removeClass("navbar-slide").addClass('navbar-transparent');
-        }
-    });
+    removeit();
+    validate_navegation();
 
     $(".fancybox").click(function(){
         $("#fancybox").show("fast");
@@ -19,3 +11,27 @@ $(function() {
         location.reload(true);
     });
 });
+
+
+function removeit() {
+    if ($(window).width() < 600) {
+        $('.video').remove();
+    }
+}
+
+function validate_navegation(){
+    var header = $("#principal-nav");
+    if ($(window).width() > 400) {
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+            if (scroll >= 100) {
+                header.removeClass('navbar-transparent').addClass("navbar-slide");
+            } else {
+                header.removeClass("navbar-slide").addClass('navbar-transparent');
+            }
+        });
+    }
+    else{
+        header.removeClass('navbar-transparent').addClass("navbar-slide");
+    }
+}
